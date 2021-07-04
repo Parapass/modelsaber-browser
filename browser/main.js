e01389552514c47a74b876c0ce4b8d3e28129e24
Parapass <59424861+Parapass@users.noreply.github.com>
2021-07-04 17:59:13 -0400

    :troll:

--- browser/main.js
+++ browser/main.js
@@ -3,8 +3,8 @@ const { app, BrowserWindow } = require('electron')
 function createWindow () {
   // Create the browser window.
   let win = new BrowserWindow({
-    width: 1280,
-    height: 720,
+    width: 800,
+    height: 600,
     webPreferences: {
       nodeIntegration: true;
       frame: false
